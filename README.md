e6280228816942b5f3c38d8fdd92bcb34f09c92a
uddeshkatyayan <uddeshkatyayan@gmail.com>
2019-11-11 04:37:39 +0530

    random commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 drive link: https://drive.google.com/drive/folders/1AboBj4w5qnL9vRHR9YEkXUB_pylJ-_OJ?usp=sharing
+
+From the direction of the camera:
+Thumb is Right -> previous label==1
+Thumb is Left -> next label==2
+Palm -> stop label==3
+Nothing==0
---
+++ frames/2019-11-07-221042_center/create_labels.py
@@ -0,0 +1,16 @@
+import sys
+
+text = ''
+filename = sys.argv[1]
+with open(filename) as f:
+    for line in f.readlines():
+    	if ".jpg" in line:
+    	   if "right" in line:
+    	   	label = '1'
+    	   elif "right" in line:
+    	   	label = '2'
+    	   elif "center" in line:
+    	   	label = '3'
+    	   text += '"'+line[:-1]+'", '+label+'\n'
+with open(filename[:-4]+'_out.csv','w+') as f:
+	f.write(text)
